056b6d11bc05fd9ccbf598301b9f1494b0bd663a
Radivoje Katanic <rkatanic@outlook.com>
2022-12-29 00:08:48 +0100

    Adds go back button

--- __tests__/components/Layout.test.tsx
+++ __tests__/components/Layout.test.tsx
@@ -1,11 +1,11 @@
 import Layout from "@/components/Layout";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 const useRouter = jest.spyOn(require("next/router"), "useRouter");
 
 describe("Layout", (): void => {
   it("should render", (): void => {
-    (useRouter as jest.Mock).mockReturnValue({ asPath: "/" });
+    (useRouter as jest.Mock).mockReturnValue({ asPath: "/", pathname: "/" });
     const { baseElement } = render(
       <Layout>
         <div>children</div>
@@ -13,5 +13,39 @@ describe("Layout", (): void => {
     );
 
     expect(baseElement).toMatchSnapshot();
+  });
+
+  describe("given project page", (): void => {
+    it("should render with go back button", (): void => {
+      (useRouter as jest.Mock).mockReturnValue({
+        asPath: "/projects",
+        pathname: "/projects/[slug]",
+      });
+      const { baseElement } = render(
+        <Layout>
+          <div>children</div>
+        </Layout>
+      );
+
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it("should navigate to go back button", (): void => {
+      const mockPush = jest.fn();
+      (useRouter as jest.Mock).mockReturnValue({
+        asPath: "/projects",
+        pathname: "/projects/[slug]",
+        push: mockPush,
+      });
+      const { getByTestId } = render(
+        <Layout>
+          <div>children</div>
+        </Layout>
+      );
+
+      fireEvent.click(getByTestId("go-back-icon"));
+
+      expect(mockPush).toHaveBeenCalledWith("/projects");
+    });
   });
 });
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,44 @@
+import { useRouter } from "next/router";
+import IconButon from "./IconButton";
 import Menu from "./Menu";
 import ThemeSwitch from "./ThemeSwitch";
+import { TbArrowBackUp } from "react-icons/tb";
 
 interface Props {
   children: JSX.Element;
 }
 
-const Layout = ({ children }: Props): JSX.Element => (
-  <div className="z-10 flex min-h-screen items-start justify-center gap-8 bg-slate-50 py-16 px-8  dark:bg-gray-900">
-    <Menu />
-    <div className="z-10 flex gap-8">
-      <div className="w-full max-w-xl"> {children}</div>
-      <div className="sticky top-16 flex h-full flex-col gap-4">
-        <ThemeSwitch />
+const Layout = ({ children }: Props): JSX.Element => {
+  const router = useRouter();
+  const isProjectPage = router.pathname.includes("/projects/[slug]");
+
+  const navigateToProjectsPage = (): void => {
+    router.push("/projects");
+  };
+
+  return (
+    <div className="z-10 flex min-h-screen items-start justify-center gap-8 bg-slate-50 py-16 px-8  dark:bg-gray-900">
+      <Menu />
+      <div className="z-10 flex gap-8">
+        <div className="w-full max-w-xl"> {children}</div>
+        <div className="sticky top-16 flex h-full flex-col gap-3">
+          <ThemeSwitch />
+          {isProjectPage && (
+            <IconButon
+              onClick={navigateToProjectsPage}
+              icon={
+                <TbArrowBackUp
+                  data-testid="go-back-icon"
+                  className="stroke-gray-400 group-hover:stroke-gray-900 dark:group-hover:stroke-white"
+                />
+              }
+              tooltip="Go Back"
+            />
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Layout;
