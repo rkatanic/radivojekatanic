b2f9050f69c6de5f4d67bb51ae180dbcb19de1f6
Radivoje Katanic <rkatanic@outlook.com>
2023-01-20 23:26:47 +0100

    Adds profile image

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,30 @@
+import Image from "next/image";
 import { FiCalendar, FiDownloadCloud } from "react-icons/fi";
 
 const Home = (): JSX.Element => (
   <div>
     <div className="-mt-8 flex items-center gap-4">
-      <div className="h-32 w-32 rounded-full border-4 border-white bg-gray-900 shadow-md"></div>
+      <div className="h-32 w-32 overflow-hidden rounded-full border-4 border-white bg-slate-200 shadow-md">
+        <Image alt="me" src="/me.png" width={240} height={240} />
+      </div>
       <div className="mt-8 flex-1">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-3xl font-semibold leading-10">
             Radivoje Katanić
           </h1>
           <div className="flex items-start gap-2">
-            <button className="flex h-9 items-center gap-2 rounded-lg  border bg-gray-800 px-3 text-sm font-medium text-white shadow-sm hover:bg-gray-900 hover:shadow-md">
-              <FiDownloadCloud /> Download CV
-            </button>
+            <a
+              href="https://drive.google.com/file/d/1SYQCLxy7JCt7TJfHK5h1G7VqL4mhM8NM/view"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button
+                type="button"
+                className="flex h-9 items-center gap-2 rounded-lg  border bg-gray-800 px-3 text-sm font-medium text-white shadow-sm hover:bg-gray-900 hover:shadow-md"
+              >
+                <FiDownloadCloud /> Download CV
+              </button>
+            </a>
           </div>
         </div>
         <h3 className="leading-6 text-gray-600">Software Developer</h3>
@@ -94,7 +106,7 @@ const Home = (): JSX.Element => (
           </div>
           <p className="my-2 font-medium text-gray-600">ProductDock</p>
           <p className="leading-6 text-gray-600">
-            After successfull bootcamp I got full time offer, where I'm
+            After successfull bootcamp I got full time offer, where I&apos;m
             currently building web products for various clients using modern web
             technologies.
           </p>
